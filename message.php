aae85a0729a2740922fe1432b45063c49ed08774
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-26 12:25:51 +0700

    Add files via upload

---
+++ message.php
@@ -0,0 +1,74 @@
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
+
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+	$replyToken = $deCode['events'][0]['replyToken'];
+
+	$messages = null;
+	$messages['replyToken'] = $replyToken;
+	$messages['messages'][0] = getFormatTextMessage("test");
+
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
+
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+
+
+	function getFormatTextMessage($text)
+	{
+		$datas = null;
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = null;
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
+?>
