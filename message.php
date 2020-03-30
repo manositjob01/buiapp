df51cd0119f705a3c2002a7a177527cd0b4b88e7
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-30 15:36:10 +0700

    Update message.php

--- message.php
+++ message.php
@@ -49,7 +49,7 @@
 	$results = sentMessage($encodeJson,$LINEDatas);
 		}
 	}else{
-		$messages['messages'][0] = getFormatTextMessage("cannot api ".$deCode["events"][0]["message"]["text"]);
+		$messages['messages'][0] = getFormatTextMessage($deCode["events"][0]["message"]["text"]);
 	$encodeJson = json_encode($messages);	
 	$results = sentMessage($encodeJson,$LINEDatas);
 	}
