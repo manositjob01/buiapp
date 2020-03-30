3f8732b576e15ebc35574799f610b0b623ccacf4
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-30 10:00:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,84 @@
 <?php
-echo "ok";
+	
+	$messages = [];
+	$messages['replyToken'] ="57aaf9f77b4a408f84d30d52d72da15f";
+	$messages['messages'][0] = "aaaa";"
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
+
+  	$results = sentMessage($encodeJson,$LINEDatas);
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+	function getFormatTextMessage($text)
+	{
+		//$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function SendAPI($server,$username,$accessKey){
+	$urlAPI="http://58.181.144.100:9081/api/";
+	$url = $urlAPI."api/RegisterToken?server=$server";
+	$param ="username=$username&accessToken=$accessKey";
+	$agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)";
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+	$result = curl_exec($ch);
+	curl_close ($ch);
+	
+
+	}
+
+	
+
+	function sentMessage($encodeJson,$datas)
+	{
+		//$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
 ?>
