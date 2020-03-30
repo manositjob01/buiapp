35c1b3592563a3f884f87d6348092ae3e2847b74
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-30 09:32:37 +0700

    Update message.php

--- message.php
+++ message.php
@@ -29,13 +29,14 @@
 
 	}
 	
-	$messages['messages'][0] = getFormatTextMessage("Register Complete");
-	$encodeJson = json_encode($messages);
 
 	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
   	$LINEDatas['token'] = "oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
 	if($flage_status["doAPI"]){
 		if($flage_status["status"]==true){
+			
+	$messages['messages'][0] = getFormatTextMessage("Register Complete");
+	$encodeJson = json_encode($messages);
   	$results = sentMessage($encodeJson,$LINEDatas);
 		}else{
 		$messages['messages'][0] = getFormatTextMessage("User ID Not Invalite");
