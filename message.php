07d3e51ca444a09e22aeecd4d8cbc624b07115c7
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-30 09:35:20 +0700

    Update message.php

--- message.php
+++ message.php
@@ -38,11 +38,11 @@
 	if($flage_status["doAPI"]){
 		if($flage_status["status"]==true){
 			
-	$messages['messages'][0] = getFormatTextMessage("Register Complete");
+	$messages['messages'][0] = getFormatTextMessage("Register Complete @".$flage_status["message"]);
 	$encodeJson = json_encode($messages);
   	$results = sentMessage($encodeJson,$LINEDatas);
 		}else{
-		$messages['messages'][0] = getFormatTextMessage("User ID Not Invalite");
+		$messages['messages'][0] = getFormatTextMessage("User ID Not Invalite @".$flage_status["message"]);
 	$encodeJson = json_encode($messages);	
 	$results = sentMessage($encodeJson,$LINEDatas);
 		}
