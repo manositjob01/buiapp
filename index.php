f3b63bc81e6b1e460e84d9f31b437f9319e0b059
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-30 10:28:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   	$LINEDatas['token'] = "oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
 
   	$results = sentMessage($encodeJson,$LINEDatas);
+	echo $results;
 	/*Return HTTP Request 200*/
 	http_response_code(200);
 
