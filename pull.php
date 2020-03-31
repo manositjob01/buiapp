e7cc10af270b385b6e7ef0005924d7fec02ee776
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-31 07:56:04 +0700

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,14 +1,13 @@
 <?php
-	if(isset($_POST["message"])){
 	
 	$arrayHeader = array();
 	$accessToken="oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
    	$arrayHeader[] = "Content-Type: application/json";
    	$arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-	$arrayPostData['to'] = $_POST["userid"];
+	$arrayPostData['to'] = $_GET["userid"];
           $arrayPostData['messages'][0]['type'] = "text";
-          $arrayPostData['messages'][0]['text'] = $_POST["message"];
+          $arrayPostData['messages'][0]['text'] = $_GET["message"];
 	 $result = pushMsg($arrayHeader,$arrayPostData);
 
 	function pushMsg($arrayHeader,$arrayPostData){
@@ -26,5 +25,5 @@
 	return $result;
    }
 exit();
-	}
+
 ?>
