bbd90ff3295a26cdd93c2fb516cf51936215892b
manositjob01 <62681959+manositjob01@users.noreply.github.com>
2020-03-31 07:49:24 +0700

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,14 +1,14 @@
 <?php
-	//if(isset($_POST["message"])){
-	//$dataItem = json_decode($_POST["message"]);
+	if(isset($_POST["message"])){
+	$dataItem = json_decode($_POST["message"]);
 	$arrayHeader = array();
 	$accessToken="oPv+uZJTdZLoNa+edPtGTj0bjhaoA3/6KaHl3BZ4THohXrD8MMtnDLgVzb5SCopNp8PbNF9RlIAn664eMDnwvhafX3pwFjeks35MMRxw/9NErEY1UOyQ/Qhj1pRMV5GFbQq/3XtRfNk9T0oF2H3hPAdB04t89/1O/w1cDnyilFU=";
    	$arrayHeader[] = "Content-Type: application/json";
    	$arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-	$arrayPostData['to'] = "U907ac541a01abf11389dbcdc8366f153";
+	$arrayPostData['to'] = $dataItem["userid"];
           $arrayPostData['messages'][0]['type'] = "text";
-          $arrayPostData['messages'][0]['text'] = "hello";
+          $arrayPostData['messages'][0]['text'] = $dataItem["message"];
 	 $result = pushMsg($arrayHeader,$arrayPostData);
 
 	function pushMsg($arrayHeader,$arrayPostData){
